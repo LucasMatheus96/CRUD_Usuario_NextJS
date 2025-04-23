b3fa9a41b031f5af5f89bcda2b80b42ee8a5f245
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-23 00:26:18 -0300

    Melhorias do componente tabela

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -5,10 +5,12 @@ import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
 
 const inter = Inter({ subsets: ['latin'] })
+
 const clientes = [
   new Cliente('Ana', 23, '1'),
   new Cliente('Bia', 43, '2'),
 ]
+
 export default function Home() {
   return (
     <div className={`
