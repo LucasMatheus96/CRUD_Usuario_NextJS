4a01a314070881a62b050ef47f0bccf56c576b08
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-25 00:46:10 -0300

    Criação do componente botão novo usuario

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
+import Botao from '../components/Botao'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,8 +29,11 @@ export default function Home() {
     text-white
     `
     }>
-
       <Layout titulo="Cadastro Simples">
+        <div className='flex justify-end'>
+        <Botao cor='green'  className='mb-4'>Novo Cliente</Botao>
+        </div>
+          
         <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
       </Layout>
       
--- next-crud/tailwind.config.js
+++ next-crud/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'from-green-400',
+    'from-blue-400',
+    'from-gray-400',
+    'to-gray-700',
+    'to-blue-700',
+    'to-green-700',
+    
+  ],
   theme: {
     extend: {
       backgroundImage: {
